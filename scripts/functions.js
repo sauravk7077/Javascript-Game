8499f51b444f767ddb75a000b1619780e4800e4d
sauravk7077 <sauravk7077@gmail.com>
2020-06-21 13:30:40 +0530

    jump using barrel

--- scripts/functions.js
+++ scripts/functions.js
@@ -14,25 +14,41 @@ class Vector{
 }
 
 class Thing{
-    constructor(position, size){
+    constructor(position, size, angle= 0){
+        this.barrel = 0;
         this.position = position;
         this.size = size;
+        this.angle = angle; //This angle is in degrees
         this.move = function(x,y) {
             this.position.add(x,y);
         };
         this.draw = function() {
+            ctx.rotate(this.angle * Math.PI/180);
             ctx.fillRect(this.position.x, this.position.y, this.size.x, this.size.y);
         };
         this.update = function(){
             if(keyPressed != null) {
-                if(keyPressed == 'ArrowUp')
+                if(keyPressed == 'ArrowUp' && this.position.y > 0)
                     this.move(0,-2);
-                else if(keyPressed == 'ArrowDown')
+                else if(keyPressed == 'ArrowDown' && this.position.y + this.size.y < canvas.height)
                     this.move(0, 2);
-                else if(keyPressed == 'ArrowLeft')
+                else if(keyPressed == 'ArrowLeft' && this.position.x > 0)
                     this.move(-2,0);
-                else if(keyPressed == 'ArrowRight')
-                    this.move(2,0);    
+                else if(keyPressed == 'ArrowRight' && this.position.x + this.size.x < canvas.width)
+                    this.move(2,0);
+                else if(keyPressed == ' ' && this.barrel == 0)
+                    this.barrel = 90;
+                else if(keyPressed == 'r')
+                    this.angle = 5;
+            }
+            if(this.barrel != 0){
+                if(this.barrel > 50){
+                    this.move(0, -10);
+                }
+                else if(this.barrel < 50){
+                    this.move(0,10);
+                }
+                this.barrel -= 10;
             }
         }
     }
@@ -43,7 +59,7 @@ let keyPressed = null;
 //functions
 function storeEvent(e) {
     keyPressed = e.key;
-    console.log(keyPressed);
+    console.log(e);
 }
 function removeEvent(e) {
     keyPressed = null;
