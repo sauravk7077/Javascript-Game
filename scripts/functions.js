86d86f27eac8f6e822ec8f049a495fdc94c3a599
sauravk7077 <sauravk7077@gmail.com>
2020-06-22 10:08:03 +0530

    added gravity, controls

--- scripts/functions.js
+++ scripts/functions.js
@@ -5,50 +5,55 @@ class Vector {
         this.x = x;
         this.y = y;
     }
-    add(a, b) {
-        this.x += a;
-        this.y += b;
+    add(vector) {
+        this.x += vector.x;
+        this.y += vector.y;
     }
 }
 class Thing {
-    constructor(position, size, angle = 0) {
+    constructor(position, size, angle = 0, velocity = new Vector(0, 0)) {
         this.barrel = 0;
         this.position = position;
         this.size = size;
         this.angle = angle; //This angle is in degrees
+        this.velocity = velocity;
     }
-    move(a, b) {
-        this.position.add(a, b);
+    move(vector) {
+        this.position.add(vector);
     }
     draw() {
         ctx.fillStyle = '#000';
         ctx.rotate(this.angle * Math.PI / 180);
         ctx.fillRect(this.position.x, this.position.y, this.size.x, this.size.y);
     }
+    update() { } //For inheritance
+}
+class GravityObject extends Thing {
+    constructor(position, size, angle = 0, isPlayer = false) {
+        super(position, size, angle);
+        this.gravity = 1;
+        this.isGravityEnabled = true;
+        this.isPlayer = isPlayer;
+    }
+    actGravity() {
+        if (this.isGravityEnabled)
+            this.velocity.add(new Vector(0, this.gravity));
+    }
     update() {
-        if (keyPressed != null) {
-            if (keyPressed == 'ArrowUp' && this.position.y > 0)
-                this.move(0, -2);
-            else if (keyPressed == 'ArrowDown' && this.position.y + this.size.y < canvas.height)
-                this.move(0, 2);
-            else if (keyPressed == 'ArrowLeft' && this.position.x > 0)
-                this.move(-2, 0);
-            else if (keyPressed == 'ArrowRight' && this.position.x + this.size.x < canvas.width)
-                this.move(2, 0);
-            else if (keyPressed == ' ' && this.barrel == 0)
-                this.barrel = 90;
-            else if (keyPressed == 'r')
-                this.angle = 5;
+        if (!this.inGround()) {
+            this.actGravity();
+            this.move(this.velocity);
         }
-        if (this.barrel != 0) {
-            if (this.barrel > 50) {
-                this.move(0, -10);
-            }
-            else if (this.barrel < 50) {
-                this.move(0, 10);
-            }
-            this.barrel -= 10;
+        //Player Controls
+        if (this.isPlayer) {
+            if (keyPressed == 'ArrowRight')
+                this.position.add(new Vector(2, 0));
+            else if (keyPressed == 'ArrowLeft')
+                this.position.add(new Vector(-2, 0));
         }
+    }
+    inGround() {
+        return false;
     }
 }
 //variables
@@ -66,18 +71,45 @@ window.addEventListener('keydown', storeEvent);
 window.addEventListener('keyup', removeEvent);
 //run
 let player;
+let ground;
+let gameObjects;
 function init() {
     canvas.width = innerWidth - 20;
     canvas.height = innerHeight - 20;
-    player = new Thing(new Vector(100, 100), new Vector(50, 50));
+    player = new GravityObject(new Vector(100, 100), new Vector(50, 50), 0, true);
+    ground = new GravityObject(new Vector(100, 500), new Vector(500, 50));
+    ground.isGravityEnabled = false;
+    gameObjects = [];
+    gameObjects.push(ground);
+    gameObjects.push(player);
+}
+function hasCollided(ob1, ob2) {
+    var box1 = new Vector(ob1.position.x + ob1.size.x, ob1.position.y + ob1.size.y);
+    var box2 = new Vector(ob2.position.x + ob2.size.x, ob2.position.y + ob2.size.y);
+    if (distance(ob1.position, ob2.position) == 0)
+        return true;
+    if (distance(box1, ob2.position) == 0)
+        return true;
+    if (distance(ob1.position, box2) == 0)
+        return true;
+    if (distance(box1, box2) == 0)
+        return true;
+    return false;
+}
+function distance(ob1, ob2) {
+    return Math.sqrt(Math.pow(ob1.x - ob2.x, 2) + Math.pow(ob1.y - ob2.y, 2));
 }
 function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, innerWidth, innerHeight);
     //ctx.fillStyle = 'rgba(255,255,255,0.3)';
     //ctx.fillRect(0,0, innerWidth, innerHeight);
-    player.draw();
-    player.update();
+    for (let ob of gameObjects) {
+        ob.draw();
+        ob.update();
+    }
+    if (hasCollided(gameObjects[0], gameObjects[1]))
+        console.log("true");
 }
 init();
 animate();
--- typescripts/functions.ts
+++ typescripts/functions.ts
@@ -11,9 +11,9 @@ class Vector{
         this.y = y;
     }
 
-    add(a: number,b: number):void {
-        this.x +=a;
-        this.y +=b;
+    add(vector:Vector){
+        this.x += vector.x;
+        this.y += vector.y;
     }
 }
 
@@ -23,50 +23,65 @@ class Thing{
     position: Vector;
     size: Vector;
     angle: number;
+    velocity: Vector;
 
-    constructor(position: Vector, size: Vector, angle:number=0){
+    constructor(position: Vector, size: Vector, angle:number=0, velocity=new Vector(0,0)){
         this.barrel = 0;
         this.position = position;
         this.size = size;
         this.angle = angle; //This angle is in degrees
+        this.velocity = velocity;
     }
+    
 
-    move(a: number,b: number):void {
-        this.position.add(a,b);
+    move(vector:Vector):void {
+        this.position.add(vector);
     }
     draw():void {
         ctx.fillStyle = '#000';
         ctx.rotate(this.angle * Math.PI/180);
         ctx.fillRect(this.position.x, this.position.y, this.size.x, this.size.y);
     }
-    update():void {
-        if(keyPressed != null) {
-            if(keyPressed == 'ArrowUp' && this.position.y > 0)
-                this.move(0,-2);
-            else if(keyPressed == 'ArrowDown' && this.position.y + this.size.y < canvas.height)
-                this.move(0, 2);
-            else if(keyPressed == 'ArrowLeft' && this.position.x > 0)
-                this.move(-2,0);
-            else if(keyPressed == 'ArrowRight' && this.position.x + this.size.x < canvas.width)
-                this.move(2,0);
-            else if(keyPressed == ' ' && this.barrel == 0)
-                this.barrel = 90;
-            else if(keyPressed == 'r')
-                this.angle = 5;
+    update():void{} //For inheritance
+}
+
+class GravityObject extends Thing{
+    gravity: number;
+    isGravityEnabled:boolean;
+    isPlayer:boolean;
+    constructor(position: Vector, size: Vector, angle:number=0, isPlayer:boolean=false){
+        super(position, size, angle);
+        this.gravity = 1;
+        this.isGravityEnabled = true;
+        this.isPlayer = isPlayer;
+    }
+
+    actGravity(){
+        if(this.isGravityEnabled)
+            this.velocity.add(new Vector(0,this.gravity));
+    }
+
+    update() {
+        if(!this.inGround()) {
+            this.actGravity();
+            this.move(this.velocity);
         }
-        if(this.barrel != 0){
-            if(this.barrel > 50){
-                this.move(0, -10);
-            }
-            else if(this.barrel < 50){
-                this.move(0,10);
-            }
-            this.barrel -= 10;
+        //Player Controls
+        if(this.isPlayer){
+        if(keyPressed == 'ArrowRight')
+            this.position.add(new Vector(2, 0));
+        else if(keyPressed == 'ArrowLeft')
+            this.position.add(new Vector(-2,0));
         }
     }
+    inGround():boolean {
+        return false;
+    }
 }
+
+
 //variables
-let keyPressed = null;
+let keyPressed:String = null;
 
 //functions
 function storeEvent(e:KeyboardEvent):void {
@@ -84,11 +99,36 @@ window.addEventListener('keyup', removeEvent);
 
 //run
 
-let player:Thing;
+let player:GravityObject;
+let ground:GravityObject;
+let gameObjects: Thing[];
 function init() {
     canvas.width = innerWidth -20;
     canvas.height = innerHeight -20;
-    player = new Thing(new Vector(100,100), new Vector(50,50));
+    player = new GravityObject(new Vector(100,100), new Vector(50,50), 0, true);
+    ground = new GravityObject(new Vector(100,500), new Vector(500,50));
+    ground.isGravityEnabled = false;
+    gameObjects = [];
+    gameObjects.push(ground);
+    gameObjects.push(player);
+}
+
+function hasCollided(ob1: Thing, ob2: Thing) {
+    var box1 = new Vector(ob1.position.x + ob1.size.x, ob1.position.y + ob1.size.y);
+    var box2 = new Vector(ob2.position.x + ob2.size.x, ob2.position.y + ob2.size.y);
+    if(distance(ob1.position, ob2.position) == 0)
+        return true;
+    if(distance(box1, ob2.position) == 0)
+        return true;
+    if(distance(ob1.position,box2) ==0)
+        return true;
+    if(distance(box1, box2) == 0)
+        return true;
+    return false;
+}
+
+function distance(ob1:Vector, ob2:Vector){
+    return Math.sqrt(Math.pow(ob1.x - ob2.x, 2) + Math.pow(ob1.y - ob2.y, 2));
 }
 
 function animate(){
@@ -96,8 +136,13 @@ function animate(){
     ctx.clearRect(0,0, innerWidth, innerHeight);
     //ctx.fillStyle = 'rgba(255,255,255,0.3)';
     //ctx.fillRect(0,0, innerWidth, innerHeight);
-    player.draw();
-    player.update();
+    for(let ob of gameObjects){
+        ob.draw();
+        ob.update();
+    }
+    if(hasCollided(gameObjects[0],gameObjects[1]))
+        console.log("true");
+    
 }
 
 init();
